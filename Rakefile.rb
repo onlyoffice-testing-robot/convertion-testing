b5c6c14c986d3d8ecc524226d0a5e3841d3e3f59
flaminestome <flamine@list.ru>
2016-10-10 20:09:46 +0300

    edit rake tasks and tests

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,13 +1,22 @@
 require_relative 'lib/app_manager'
 
-# rake convertconvert
 desc 'convert files from default paths'
-task :convert, :format_from, :format_to do |_t, args|
-  Converter.new(:convert_from=>'assets/files',:convert_to => 'results',:x2t_path => 'assets/x2t/x2t').convert(args[:format_from] => args[:format_to])
+# rake convert[doc,docx,configure_file_path]
+task :convert_without_config, :format_from, :format_to do |_t, args|
+  Converter.new(:convert_from => 'assets/files', :convert_to => 'results', :x2t_path => 'assets/x2t/x2t').convert(args[:format_from] => args[:format_to])
 end
+
+task :convert_with_config, :format_from, :format_to, :configure_file do |_t, args|
+  Converter.new(:configure=>args[:configure_file]).convert(args[:format_from] => args[:format_to])
+end
+
 
 desc 'convert files from custom configure.json'
 # rake convert[doc,docx,configure_file_path]
-task :convert_with_config, :format_from, :format_to, :configure_file do |_t, args|
-  Converter.new(:configure=>args[:configure_file]).convert(args[:format_from] => args[:format_to])
+task :convert, :format_from, :format_to, :configure_file do |_t, args|
+  if args[:configure_file].nil?
+    Rake::Task['convert_without_config'].invoke(args[:format_from], args[:format_to])
+  else
+    Rake::Task['convert_with_config'].invoke(args[:format_from], args[:format_to], args[:configure_file])
+  end
 end
--- spec/rake_spec.rb
+++ spec/rake_spec.rb
@@ -12,9 +12,9 @@ load "#{Dir.pwd}/Rakefile.rb"
   end
 
   it 'check convert_with_config rake task' do
-    result_dir_count = Dir['results/*'].length
-    Rake::Task['convert_with_config'].invoke(:doc, :docx, 'configure.json')
+    result_dir_count = Dir['spec/test_data/tmp/*'].length
+    Rake::Task['convert'].invoke(:doc, :docx, 'spec/test_data/configure.json')
     sleep 1 # wait for file/folder created
-    expect(result_dir_count).to eq(Dir['results/*'].length - 1)
+    expect(result_dir_count).to eq(Dir['spec/test_data*'].length - 1)
   end
 end
