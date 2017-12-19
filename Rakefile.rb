8cebd230d638dbad6600ce66c823a67ef5b832df
flaminestone <kvazilife@gmail.com>
2017-12-19 17:22:28 +0300

    Change structure of project. Now - for use need only one task - rake convert

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,38 +1,6 @@
 require_relative 'lib/app_manager'
-
-desc 'convert files from default paths'
-# rake convert[doc,docx,configure_file_path]
-task :convert_without_config, :format_from, :format_to do |_t, args|
-  Converter.new(:convert_from => 'assets/files', :convert_to => 'results', :x2t_path => 'assets/x2t/x2t').convert(args[:format_from] => args[:format_to])
-end
-
-task :convert_with_config, :format_from, :format_to, :configure_file do |_t, args|
-  Converter.new(:configure=>args[:configure_file]).convert(args[:format_from] => args[:format_to])
-end
-
 desc 'convert files from custom configure.json'
-# rake convert[doc,docx,configure_file_path]
-task :convert, :format_from, :format_to, :configure_file do |_t, args|
-  if args[:configure_file].nil?
-    Rake::Task['convert_without_config'].invoke(args[:format_from], args[:format_to])
-  else
-    Rake::Task['convert_with_config'].invoke(args[:format_from], args[:format_to], args[:configure_file])
-  end
-end
-
-task :convert_all_formats do
-  #doc to docx
-  result = []
-  {:doc=>:docx, :rtf=>:docx, :docx=>:rtf, :odt=>:docx, :docx=>:odt, :xls=>:xlsx, :ods => :xlsx, :xlsx => :ods, :ppt=>:pptx, :odp => :pptx}.each do |current_key, current_value|
-    ourput_path = Converter.new(:configure=>'configure.json').convert(current_key => current_value)
-    if FileHelper.list_file_in_directory("#{ourput_path}/not_converted").empty?
-        FileHelper.delete_directory(ourput_path)
-      else
-      result << ourput_path
-    end
-  end
-  unless result.empty?
-    puts "Not converted:"
-    p result
-  end
+# rake convert
+task :convert do |_t|
+  Converter.new.convert
 end
--- lib/convertion/converter.rb
+++ lib/convertion/converter.rb
@@ -1,39 +1,19 @@
 require_relative '../app_manager'
-
+require 'yaml'
+# use Converter.new.convert for convert by config
 class Converter
-  # @param args must contain hash with keys :base_file_folder, :output_folder, and :bin_path
-  # Values for this keys - is [String] - path to folder with files for converting (:base_file_folder), path to folder for results (output_folder) and
-  # path to x2t file(:bin_path).
-  # Example:
-  # Converter.new( :convert_from => "assets/files", :convert_to => "results", :x2t_path => "assets/x2t/x2t", :font_path => "assets/fonts")
-  def initialize(*args)
-    if args.first[:configure]
-      use_config(args.first[:configure])
-    else
-      @convert_from = args.first[:convert_from]
-      @convert_to = args.first[:convert_to]
-      @bin_path = args.first[:x2t_path]
-      @font_path = args.first[:font_path]
-      @convert_from.chop! if @convert_from.rindex('/') == (@convert_from.size - 1)
-      @convert_to.chop! if @convert_to.rindex('/') == (@convert_to.size - 1)
-    end
-  end
-
-  def use_config(configure_file)
-    config_string = FileHelper.read_file_to_string(configure_file).delete("\n")
-    config_string.delete!("\n")
-    config_hash = JSON.parse(config_string)
-    @convert_from = config_hash['convert_from']
-    @convert_to = config_hash['convert_to']
-    @bin_path = config_hash['x2t_path']
-    @font_path = config_hash['font_path']
-    config_hash
+  def initialize
+    config = YAML.load_file('configure.json')
+    @convert_from = config['convert_from']
+    @convert_to = config['convert_to']
+    @bin_path = config['x2t_path']
+    @font_path = config['font_path']
+    @output_format = config['format']
   end
 
   # @param [String] path is a path to folder
-  # @param [String] extension find marker. Method will find only files with this word
-  def get_file_paths_list(path, extension = nil)
-    FileHelper.list_file_in_directory(path, extension)
+  def get_file_paths_list(path)
+    FileHelper.list_file_in_directory(path)
   end
 
   # @param [String] folder_name - name for new folder
@@ -42,66 +22,67 @@ class Converter
     FileHelper.create_folder("#{folder_name}/not_converted")
   end
 
-  # @param [Array] base_file_list - first list of file names
-  # @param [Array] last_file_list - second list of file names
-  def get_file_difference(base_file_list, last_file_list)
-    input_file_names = base_file_list.map { |current_path| File.basename(current_path, '.*') }
-    output_file_names = last_file_list.map { |current_path| File.basename(current_path, '.*') }
-    not_converted = input_file_names - output_file_names
-    input_file_format = File.extname(base_file_list.first)
-    not_converted_full_name = not_converted.map { |file_name|
-      `find #{@convert_from} -name "#{file_name}#{input_file_format}"`.chomp
-    }
-    not_converted_full_name.each do |not_converted_file|
-      begin
-        FileHelper.copy_file(not_converted_file, "#{@output_folder}/not_converted")
-      rescue
-        LoggerHelper.print_to_log "#{not_converted_file} not copy"
-      end
-      not_converted_full_name
-    end
-  end
-
   # @param [String] input_filename - input filename with format
-  # @param [String] output_filename - input filename with format
-  def convert_file(input_filename, output_filename, csv_log = true)
-    LoggerHelper.print_to_log "Start convert file #{input_filename} to #{output_filename}"
-    command = "\"#{@bin_path}\" \"#{input_filename}\" \"#{output_filename}\" \"#{@font_path}\""
+  def convert_file(input_filename)
+    output_filepath = get_output_filepath(input_filename)
+    LoggerHelper.print_to_log "Start convert file #{input_filename} to #{output_filepath}"
+    command = "\"#{@bin_path}\" \"#{input_filename}\" \"#{output_filepath}\" \"#{@font_path}\""
     LoggerHelper.print_to_log "Run command #{command}"
-    a = Time.now
+    time_before = Time.now
     `#{command}`
-    a = Time.now - a
-    File.open("#{@output_folder}/results.csv", 'a') { |file| file.write "#{File.basename(input_filename)};#{FileHelper.file_size(input_filename) / 1000 / 1000.0};#{a};\n" } if csv_log
+    time = Time.now - time_before
+    check_file_exist(input_filename, output_filepath, time)
     LoggerHelper.print_to_log 'End convert'
     puts '--' * 75
   end
 
-  # :input_file => 'assets/files/1.xls', :format_to => 'xlsx'
-  def convert_one(*args)
-    raise "x2t file is not found in #{@bin_path} path" unless x2t_exist?
-    output_file_path = "#{@convert_to}/#{File.basename(args.first[:input_file], '.*')}." + args.first[:format_to]
-    convert_file(args.first[:input_file], output_file_path, false)
-    output_file_path
+  def check_file_exist(input_filename, output_filepath, time)
+    if File.exist?(output_filepath)
+      File.open("#{@output_folder}/results.csv", 'a') do |file|
+        file.write "#{file_name(input_filename)};#{file_size(input_filename)};#{time};true;\n"
+      end
+    else
+      File.open("#{@output_folder}/results.csv", 'a') do |file|
+        file.write "#{file_name(input_filename)};#{file_size(input_filename)};#{time};false;\n"
+      end
+      FileHelper.copy_file(input_filename, "#{@output_folder}/not_converted")
+    end
+  end
+
+  def file_name(input_filename)
+    File.basename(input_filename)
+  end
+
+  def file_size(input_filename)
+    FileHelper.file_size(input_filename) / 1000 / 1000.0
   end
 
   def x2t_exist?
     File.exist?(@bin_path)
   end
 
-  # @param [Hash] option_hash. Key - is a start format, value - result format
-  def convert(option_hash)
-    raise "x2t file is not found in #{@bin_path} path" unless x2t_exist?
-    @input_format = option_hash.keys.first
-    @output_format = option_hash.values.first
-    @output_folder = "#{@convert_to}/#{@input_format}_to_#{@output_format}_by_#{Time.now.strftime('%d_%b_%Y_%H:%M:%S')}"
+  def get_output_filepath(filepath)
+    @output_folder + '/' + File.basename(filepath, '.*') + '.' + @output_format
+  end
+
+  def check_macros(file)
+    # command = "\"#{@bin_path}\" -detectmacro \"#{file}\""
+    system "\"#{@bin_path}\" -detectmacro \"#{file}\""
+    $CHILD_STATUS.exitstatus != 0
+  end
+
+  def convert
+    @output_folder = "#{@convert_to}/result_#{@output_format}_by_#{Time.now.strftime('%d_%b_%Y_%H:%M:%S')}"
     create_folder @output_folder
-    File.open("#{@output_folder}/results.csv", 'w') { |file| file.write "filename;filesize(kbytes);time(sec)\n" }
-    file_list = get_file_paths_list(@convert_from, @input_format)
-    file_list.each do |current_file_to_convert|
-      output_file_path = "#{@output_folder}/#{File.basename(current_file_to_convert, '.*')}.#{@output_format}"
-      convert_file(current_file_to_convert, output_file_path)
+    File.open("#{@output_folder}/results.csv", 'w') { |file| file.write "filename;filesize(kbytes);time(sec);convert_status\n" }
+    get_file_paths_list(@convert_from).each do |current_file_to_convert|
+      p current_file_to_convert
+      if (@output_format == ('docm' || 'xlsm' || 'pptm')) && check_macros(current_file_to_convert)
+        p "Skip #{current_file_to_convert} because it has no macros for #{@output_format}"
+        next
+      end
+      convert_file(current_file_to_convert)
     end
-    get_file_difference(file_list, get_file_paths_list(@output_folder, @output_format))
     @output_folder
   end
 end
