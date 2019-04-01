739340f0086b5d8e833fb10e1dac6779e1c734af
Dmitriy Rotatyy <flamine@list.ru>
2019-04-01 15:06:52 +0300

    Change clear method for deleting subfolders too

--- framework/file_helper.rb
+++ framework/file_helper.rb
@@ -1,8 +1,6 @@
 class FileHelper
   def self.clear_tmp
     OnlyofficeLoggerHelper.log('Clear tmp dir')
-    Dir["#{StaticData::TMP_DIR}/*"].each do |filepath|
-      File.delete(filepath) if File.exist?(filepath)
-    end
+    FileUtils.rm_rf(Dir.glob('tmp/*'))
   end
 end
