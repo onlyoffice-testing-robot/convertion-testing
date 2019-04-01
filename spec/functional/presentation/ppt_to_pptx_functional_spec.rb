64816f1c6138b91ee107b6dbab4b7e6bcf7fa3b4
Dmitriy Rotatyy <flamine@list.ru>
2019-04-01 15:55:43 +0300

    Add new spec for testing ppt-pptx conversion

---
+++ spec/functional/presentation/ppt_to_pptx_functional_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+s3 = OnlyofficeS3Wrapper::AmazonS3Wrapper.new
+palladium = PalladiumHelper.new(X2t.new.version, 'Ppt to Pptx')
+result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
+files = s3.get_files_by_prefix('ppt')
+file_data = nil
+describe 'Conversion ppt files to pptx' do
+  (files - result_sets.map { |result_set| "doc/#{result_set}" }).each do |file|
+    it File.basename(file) do
+      s3.download_file_by_name(file, StaticData::TMP_DIR)
+      file_data = X2t.new.convert("#{StaticData::TMP_DIR}/#{File.basename(file)}", :pptx)
+      expect(File.exist?(file_data[:tmp_filename])).to be_truthy
+    end
+  end
+
+  after :each do |example|
+    FileHelper.clear_tmp
+    palladium.add_result(example, file_data)
+  end
+end
