039b3daa12f7417ebb95ebf41329a30c273e884f
Dmitriy Rotatyy <flamine@list.ru>
2019-04-09 16:42:15 +0300

    Add ooxml parser to project for make check for file contain dta after conversions

--- management.rb
+++ management.rb
@@ -5,6 +5,7 @@ require_relative 'framework/file_helper'
 require_relative 'framework/palladium_helper'
 require 'onlyoffice_s3_wrapper'
 require 'onlyoffice_logger_helper'
+require 'ooxml_parser'
 require 'palladium'
 require 'onlyoffice_tcm_helper'
 
--- spec/functional/spreadsheets/xls_to_xlsx_functional_spec.rb
+++ spec/functional/spreadsheets/xls_to_xlsx_functional_spec.rb
@@ -10,6 +10,7 @@ describe 'Conversion xls files to xlsx' do
       s3.download_file_by_name(file, StaticData::TMP_DIR)
       file_data = X2t.new.convert("#{StaticData::TMP_DIR}/#{File.basename(file)}", :xlsx)
       expect(File.exist?(file_data[:tmp_filename])).to be_truthy
+      expect(OoxmlParser::Parser.parse(file_data[:tmp_filename])).to be_with_data
     end
   end
 
