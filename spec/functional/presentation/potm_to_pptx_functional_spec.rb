539a223b30895ff68d9946793a263bdd5ca80289
Dmitriy Rotatyy <flamine@list.ru>
2019-04-12 14:50:35 +0300

    Add tests for conversions: fodp-pptx pot-pptx potm-pptx potx-pptx pps-pptx ppsm-pptx ppsx-pptx pptm-pptx

---
+++ spec/functional/presentation/potm_to_pptx_functional_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+s3 = OnlyofficeS3Wrapper::AmazonS3Wrapper.new
+palladium = PalladiumHelper.new(X2t.new.version, 'Potm to Pptx')
+result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
+files = s3.get_files_by_prefix('potm/')
+file_data = nil
+describe 'Conversion potm files to pptx' do
+  (files - result_sets.map { |result_set| "potm/#{result_set}" }).each do |file|
+    it File.basename(file) do
+      s3.download_file_by_name(file, StaticData::TMP_DIR)
+      file_data = X2t.new.convert("#{StaticData::TMP_DIR}/#{File.basename(file)}", :pptx)
+      expect(File.exist?(file_data[:tmp_filename])).to be_truthy
+      expect(OoxmlParser::Parser.parse(file_data[:tmp_filename])).to be_with_data
+    end
+  end
+
+  after :each do |example|
+    FileHelper.clear_tmp
+    palladium.add_result(example, file_data)
+  end
+end
