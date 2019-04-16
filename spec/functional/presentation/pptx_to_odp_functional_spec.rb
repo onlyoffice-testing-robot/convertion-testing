cfba44d254d8ace44d98bc0764f7799e969d8fbe
Dmitriy Rotatyy <flamine@list.ru>
2019-04-16 16:04:02 +0300

    Delete specs because this functional is testing with other tests

--- spec/functional/presentation/pptx_to_odp_functional_spec.rb
+++ spec/functional/presentation/pptx_to_odp_functional_spec.rb
@@ -1,16 +1,15 @@
 require 'rspec'
 s3 = OnlyofficeS3Wrapper::AmazonS3Wrapper.new
-palladium = PalladiumHelper.new(X2t.new.version, 'Otp to Pptx')
+palladium = PalladiumHelper.new(X2t.new.version, 'Pptx to Odp')
 result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
-files = s3.get_files_by_prefix('otp/')
+files = s3.get_files_by_prefix('pptx/')
 file_data = nil
-describe 'Conversion otp files to pptx' do
-  (files - result_sets.map { |result_set| "otp/#{result_set}" }).each do |file|
+describe 'Conversion pptx files to odp' do
+  (files - result_sets.map { |result_set| "pptx/#{result_set}" }).each do |file|
     it File.basename(file) do
       s3.download_file_by_name(file, StaticData::TMP_DIR)
-      file_data = X2t.new.convert("#{StaticData::TMP_DIR}/#{File.basename(file)}", :pptx)
+      file_data = X2t.new.convert("#{StaticData::TMP_DIR}/#{File.basename(file)}", :odp)
       expect(File.exist?(file_data[:tmp_filename])).to be_truthy
-      expect(OoxmlParser::Parser.parse(file_data[:tmp_filename])).to be_with_data
     end
   end
 
