ac466c6cb31229c8abda1e6220c3b998b42e5ab7
Dmitriy Rotatyy <flamine@list.ru>
2019-04-16 12:48:55 +0300

    Fix missprinting

--- spec/functional/presentation/pptx_to_otp_functional_spec.rb
+++ spec/functional/presentation/pptx_to_otp_functional_spec.rb
@@ -8,7 +8,7 @@ describe 'Conversion pptx files to odp' do
   (files - result_sets.map { |result_set| "otp/#{result_set}" }).each do |file|
     it File.basename(file) do
       s3.download_file_by_name(file, StaticData::TMP_DIR)
-      file_data = X2t.new.convert("#{StaticData::TMP_DIR}/#{File.basename(file)}", :odp)
+      file_data = X2t.new.convert("#{StaticData::TMP_DIR}/#{File.basename(file)}", :otp)
       expect(File.exist?(file_data[:tmp_filename])).to be_truthy
     end
   end
