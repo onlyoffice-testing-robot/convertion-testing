9b4869a35e4839466632d27d8f752ed9077f221b
Dmitriy Rotatyy <flamine@list.ru>
2019-04-18 15:34:30 +0300

    Add ability to run tests for document editor via parallels

--- spec/functional/documents/docx_to_odt_functional_spec.rb
+++ spec/functional/documents/docx_to_odt_functional_spec.rb
@@ -3,18 +3,20 @@ s3 = OnlyofficeS3Wrapper::AmazonS3Wrapper.new
 palladium = PalladiumHelper.new(X2t.new.version, 'Docx to Odt')
 result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
 files = s3.get_files_by_prefix('docx/')
-file_data = nil
 describe 'Conversion docx files to odt' do
+  before :each do
+    @tmp_dir = FileHelper.create_tmp_dir.first
+  end
   (files - result_sets.map { |result_set| "docx/#{result_set}" }).each do |file|
     it File.basename(file) do
-      s3.download_file_by_name(file, StaticData::TMP_DIR)
-      file_data = X2t.new.convert("#{StaticData::TMP_DIR}/#{File.basename(file)}", :odt)
-      expect(File.exist?(file_data[:tmp_filename])).to be_truthy
+      s3.download_file_by_name(file, @tmp_dir)
+      @file_data = X2t.new.convert("#{@tmp_dir}/#{File.basename(file)}", :odt, @tmp_dir)
+      expect(File.exist?(@file_data[:tmp_filename])).to be_truthy
     end
   end
 
   after :each do |example|
-    palladium.add_result(example, file_data)
-    FileHelper.clear_tmp
+    FileHelper.delete_tmp(@tmp_dir)
+    palladium.add_result(example, @file_data)
   end
 end
--- spec/functional/documents/rtf_to_docx_functional_spec.rb
+++ spec/functional/documents/rtf_to_docx_functional_spec.rb
@@ -3,19 +3,22 @@ s3 = OnlyofficeS3Wrapper::AmazonS3Wrapper.new
 palladium = PalladiumHelper.new(X2t.new.version, 'Rtf to Docx')
 result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
 files = s3.get_files_by_prefix('rtf/')
-file_data = nil
-describe 'Conversion rtf files to rtfx' do
+describe 'Conversion rtf files to docx' do
+  before :each do
+    @tmp_dir = FileHelper.create_tmp_dir.first
+  end
   (files - result_sets.map { |result_set| "rtf/#{result_set}" }).each do |file|
     it File.basename(file) do
-      s3.download_file_by_name(file, StaticData::TMP_DIR)
-      file_data = X2t.new.convert("#{StaticData::TMP_DIR}/#{File.basename(file)}", :docx)
-      expect(File.exist?(file_data[:tmp_filename])).to be_truthy
-      expect(OoxmlParser::Parser.parse(file_data[:tmp_filename])).to be_with_data
+      p file
+      s3.download_file_by_name(file, @tmp_dir)
+      @file_data = X2t.new.convert("#{@tmp_dir}/#{File.basename(file)}", :docx, @tmp_dir)
+      expect(File.exist?(@file_data[:tmp_filename])).to be_truthy
+      expect(OoxmlParser::Parser.parse(@file_data[:tmp_filename]).with_data?).to be_truthy
     end
   end
 
   after :each do |example|
-    palladium.add_result(example, file_data)
-    FileHelper.clear_tmp
+    FileHelper.delete_tmp(@tmp_dir)
+    palladium.add_result(example, @file_data)
   end
 end
