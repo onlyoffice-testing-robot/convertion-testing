91394f1b1a2b3dfe32d1e976539b0e2a41adb313
Dmitriy Rotatyy <flamine@list.ru>
2019-04-18 15:43:49 +0300

    Update tests for using parallels

--- spec/functional/spreadsheets/xlsx_to_ods_functional_spec.rb
+++ spec/functional/spreadsheets/xlsx_to_ods_functional_spec.rb
@@ -3,18 +3,20 @@ s3 = OnlyofficeS3Wrapper::AmazonS3Wrapper.new
 palladium = PalladiumHelper.new(X2t.new.version, 'Xlsx to Ods')
 result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
 files = s3.get_files_by_prefix('xlsx/')
-file_data = nil
 describe 'Conversion xlsx files to ods' do
+  before :each do
+    @tmp_dir = FileHelper.create_tmp_dir.first
+  end
   (files - result_sets.map { |result_set| "xlsx/#{result_set}" }).each do |file|
     it File.basename(file) do
-      s3.download_file_by_name(file, StaticData::TMP_DIR)
-      file_data = X2t.new.convert("#{StaticData::TMP_DIR}/#{File.basename(file)}", :ods)
-      expect(File.exist?(file_data[:tmp_filename])).to be_truthy
+      s3.download_file_by_name(file, @tmp_dir)
+      @file_data = X2t.new.convert("#{@tmp_dir}/#{File.basename(file)}", :ods, @tmp_dir)
+      expect(File.exist?(@file_data[:tmp_filename])).to be_truthy
     end
   end
 
   after :each do |example|
-    FileHelper.clear_tmp
-    palladium.add_result(example, file_data)
+    FileHelper.delete_tmp(@tmp_dir)
+    palladium.add_result(example, @file_data)
   end
 end
