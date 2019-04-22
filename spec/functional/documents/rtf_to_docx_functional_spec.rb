fa436c1ab461e16c12d3e2f69b354af16c3e0079
Dmitriy Rotatyy <flamine@list.ru>
2019-04-22 12:21:41 +0300

    Delete check for empty files

--- spec/functional/documents/rtf_to_docx_functional_spec.rb
+++ spec/functional/documents/rtf_to_docx_functional_spec.rb
@@ -9,11 +9,10 @@ describe 'Conversion rtf files to docx' do
   end
   (files - result_sets.map { |result_set| "rtf/#{result_set}" }).each do |file|
     it File.basename(file) do
-      p file
       s3.download_file_by_name(file, @tmp_dir)
       @file_data = X2t.new.convert("#{@tmp_dir}/#{File.basename(file)}", :docx, @tmp_dir)
       expect(File.exist?(@file_data[:tmp_filename])).to be_truthy
-      expect(OoxmlParser::Parser.parse(@file_data[:tmp_filename]).with_data?).to be_truthy
+      expect(OoxmlParser::Parser.parse(@file_data[:tmp_filename]).with_data?).to be_truthy unless StaticData::EMPTY_FILES.include?(File.basename(file))
     end
   end
 
