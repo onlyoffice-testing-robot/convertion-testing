3beaeba524d214b26aa350d2bee25f946a57f2c6
Dmitriy Rotatyy <flamine@list.ru>
2019-04-22 19:53:34 +0300

    Fix text after changing filehelper

--- spec/smoke/convertion_tests_smoke_spec.rb
+++ spec/smoke/convertion_tests_smoke_spec.rb
@@ -3,6 +3,9 @@ s3 = OnlyofficeS3Wrapper::AmazonS3Wrapper.new
 palladium = PalladiumHelper.new(X2t.new.version, 'Conversion tests smoke')
 
 describe 'Conversion tests' do
+  before :each do
+    @tmp_dir = FileHelper.create_tmp_dir.first
+  end
   StaticData::CONVERSION_STRAIGHT.each_pair do |format_from, formats_to|
     formats_to.each do |format_to|
       it "Check converting from #{format_from} to #{format_to}" do
@@ -30,11 +33,11 @@ describe 'Conversion tests' do
     file_data = X2t.new.convert(filepath, :xlst)
     expect(File.exist?(file_data[:tmp_filename])).to be_falsey
     expect(file_data[:size_after]).to be_nil
-    expect(file_data[:x2t_result]).to eq("Couldn't automatically recognize conversion direction from extensions\n")
+    expect(file_data[:x2t_result]).to eq("Couldn't automatically recognize conversion direction from extensions")
   end
 
   after :each do |example|
-    FileHelper.clear_tmp
+    FileHelper.delete_tmp(@tmp_dir)
     palladium.add_result(example)
   end
 end
