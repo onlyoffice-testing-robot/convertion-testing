7563f2d7c5eff653650c88ab12669d467f329189
Dmitriy Rotatyy <flamine@list.ru>
2020-01-21 14:57:38 +0300

    update specs for work with new lib

--- spec/functional/documents/odt_to_docx_functional_spec.rb
+++ spec/functional/documents/odt_to_docx_functional_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rspec'
 s3 = OnlyofficeS3Wrapper::AmazonS3Wrapper.new
-palladium = PalladiumHelper.new(X2t.new.version, 'Odt to Docx')
+palladium = PalladiumHelper.new(x2t.version, 'Odt to Docx')
 result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
 files = s3.get_files_by_prefix('odt/')
 describe 'Conversion odt files to docx' do
@@ -12,7 +12,7 @@ describe 'Conversion odt files to docx' do
   (files - result_sets.map { |result_set| "odt/#{result_set}" }).each do |file|
     it File.basename(file) do
       s3.download_file_by_name(file, @tmp_dir)
-      @file_data = X2t.new.convert("#{@tmp_dir}/#{File.basename(file)}", :docx, @tmp_dir)
+      @file_data = x2t.convert("#{@tmp_dir}/#{File.basename(file)}", :docx)
       expect(File.exist?(@file_data[:tmp_filename])).to be_truthy
       expect(OoxmlParser::Parser.parse(@file_data[:tmp_filename])).to be_with_data
     end
