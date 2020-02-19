a661ece44020c42d7769e57fabddf47b906c6c77
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-02-19 13:44:15 +0300

    Actualize RuboCop todo to 0.80.0 (#48)
    * Actualize RuboCop todo to 0.80.0
    
    * Fix RuboCop `Layout/EmptyLines` cop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,6 @@ def x2t
                    tmp_path: StaticData::TMP_DIR)
 end
 
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
