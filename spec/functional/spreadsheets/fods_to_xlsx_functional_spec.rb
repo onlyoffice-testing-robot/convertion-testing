ab99e2b176eb215295fe40635778b40f0cf12faa
Sorokin_Max <askonev007@gmail.com>
2020-08-26 13:46:06 +0300

    Add correct file extension

--- spec/functional/spreadsheets/fods_to_xlsx_functional_spec.rb
+++ spec/functional/spreadsheets/fods_to_xlsx_functional_spec.rb
@@ -8,7 +8,7 @@ describe 'Conversion fods files to xlsx' do
   before :each do
     @tmp_dir = FileHelper.create_tmp_dir.first
   end
-  (files - result_sets.map { |result_set| "ods/#{result_set}" }).each do |file|
+  (files - result_sets.map { |result_set| "fods/#{result_set}" }).each do |file|
     it File.basename(file) do
       s3.download_file_by_name(file, @tmp_dir)
       @file_data = x2t.convert("#{@tmp_dir}/#{File.basename(file)}", :xlsx)
